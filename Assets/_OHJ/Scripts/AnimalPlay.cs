f368f6e32c55671117892e38c0dfb16dd3917671
HyejeongOk <oghyejeong2@gmail.com>
2025-07-03 17:28:22 +0900

    v0.0.3] feat : 공 던지기 구현

---
+++ Assets/_OHJ/Scripts/AnimalPlay.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalPlay : AnimalAbility
+{
+    public override void Init()
+    {
+
+    }
+    public override void UnInit()
+    {
+
+    }
+}
