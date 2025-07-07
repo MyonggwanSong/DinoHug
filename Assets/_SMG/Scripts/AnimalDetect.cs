97da65f6d0c3b6cfbeb41089d5ea8dbfd8e7084b
MyonggwanSong <spooh01@naver.com>
2025-07-07 18:02:37 +0900

    SMG] v0.0.2 feat : Animal Detect

---
+++ Assets/_SMG/Scripts/AnimalDetect.cs
@@ -0,0 +1,83 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class AnimalDetect : MonoBehaviour
+{
+    [SerializeField] float detectRadius = 2f;
+    AnimalControl animal;
+    XRController controller;
+    [SerializeField] GameObject popUpKey;
+
+
+    void Awake()
+    {
+        animal = GetComponent<AnimalControl>();
+    }
+  
+ 
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("들어옴");
+            controller = other.GetComponentInParent<XRController>();        // 진동 효과 및 자이로스코프 활용 용
+            popUpKey.SetActive(true);
+          
+                animal.ChangeState(AnimalControl.State.Handle);
+            
+
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("나감");
+            controller = null;
+            popUpKey.SetActive(false);
+            animal.ChangeState(AnimalControl.State.Idle);
+        }
+    }
+    
+
+    // void Update()
+    // {
+    //     Collider[] cols = Physics.OverlapSphere(transform.position, detectRadius);
+    //     if (cols != null)
+    //     {
+    //         foreach (var col in cols)
+    //         {
+
+    //             if (col.CompareTag("Player"))
+    //             {
+    //                 _timer += Time.deltaTime;
+    //                 Debug.Log(_timer);  
+    //                 animal.ChangeState(AnimalControl.State.Idle);
+
+    //                 Debug.Log("들어옴");
+    //                 controller = col.GetComponent<XRController>();  // 진동 효과 및 자이로스코프 활용 용
+
+    //                 popUpKey.SetActive(true);   // 쓰다듬기 활성 가능
+    //                 if (_timer > 2f) // 들어온 시간이 2초가 넘으면 실행
+    //                 {
+    //                     _timer = 0f;
+    //                     animal.ChangeState(AnimalControl.State.Handle);
+    //                 }
+    //                 return;
+    //             }
+    //         }
+    //         Debug.Log("나감");
+    //         animal.ChangeState(AnimalControl.State.Wander);
+    //         controller = null;
+    //         popUpKey.SetActive(false);
+    //     }
+
+    // }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawSphere(transform.position, 2f);
+    }
+}
--- Assets/_SMG/Scripts/AnimalPet.cs
+++ Assets/_SMG/Scripts/AnimalPet.cs
@@ -3,90 +3,34 @@ using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
 
+
 public class AnimalPet : AnimalAbility
-{
-    [SerializeField] AnimalControl ac;
-    [SerializeField] Animator animator;
-    //[SerializeField] float detectRadius = 2f;
-    [SerializeField] GameObject popUpKey;
-
-    [SerializeField] XRController controller;
-
-    Collider[] _playerController;
-
-    void Start()
-    {
-       // popUpKey.SetActive(false);
-        anim = GetComponentInChildren<Animator>();
-        animator = anim;
-    }
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.tag == "Player")
-        {
-            Debug.Log("들어옴");
-            controller = other.GetComponentInParent<XRController>();
-            popUpKey.SetActive(true);
-        }
-    }
-    void OnTriggerExit(Collider other)
-    {
-        if (other.tag == "Player")
-        {
-            Debug.Log("나감");
-            controller = null;
-            popUpKey.SetActive(false);
-        }
-    }
-    //     void Update()
-    //     {
-    //         Collider[] cols = Physics.OverlapSphere(transform.position, detectRadius);
-    //         if (cols != null)
-    //         {
-    //             foreach (var col in cols)
-    //             {
-    //                 // _playerController = col;
-    //                 if (col.CompareTag("Player"))
-    //                 {
-    //  Debug.Log("들어옴");
-    //                     controller = col.GetComponent<XRController>();
-    //                     popUpKey.SetActive(true);
-    //                 }
-    //                 else
-    //                 {
-    //                     controller = null;
-    //                     popUpKey.SetActive(false);
-    //                 }
-
-    //                 continue;
-    //             }
-    //         }
-    //     }
+{  
+    Coroutine _co = null;
 
     public override void Init()
     {
-        StartCoroutine(Play());
+       _co = StartCoroutine( Pet());
     }
 
     public override void UnInit()
     {
-
+        StopCoroutine(_co);
     }
-
-    IEnumerator Play()
+   
+    IEnumerator Pet()
     {
         yield return null;
 
-        anim.Play("aniClip1");
         
-         Debug.Log("들어옴");
+
+        anim.Play("aniClip1");
+
+
+
+        Debug.Log("쓰다듬는 중");
+
 
     }
-    IEnumerator Pet()
-    {
-        yield return new WaitUntil(() => controller.enableInputActions);
-         Debug.Log("들어옴");
-
-
-    }
+  
 }
