f21aedd290a4b9e59c62688202f050b51c3a1a30
MyonggwanSong <spooh01@naver.com>
2025-07-08 16:39:20 +0900

    SMG] v0.0.4 feat : petting

--- Assets/_SMG/Scripts/PettingTrigger.cs
+++ Assets/_SMG/Scripts/PettingTrigger.cs
@@ -24,7 +24,7 @@ public class PettingTrigger : MonoBehaviour
     }
     void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("GameController"))
+        if (other.CompareTag("GameController") && controller == null)
         {
             controller = other.GetComponentInParent<ActionBasedController>();
 
@@ -35,73 +35,80 @@ public class PettingTrigger : MonoBehaviour
     bool isStable = false;
     void OnTriggerStay(Collider other)
     {
+        if (controller == null || controller.activateAction == null || controller.activateAction.action == null)
+            return;
         float input = controller.activateAction.action.ReadValue<float>();
         Debug.Log(input);
 
         if (input == 1)
         {
-            if (!isStable)
+            if (!isStable)                                          // 클릭시 멈춰있으면 handle로 상태 변경
             {
                 isStable = true;
                 ac.ChangeState(AnimalControl.State.Handle);
             }
 
-            // if (delay_co != null)
-            //     StopCoroutine(delay_co);
-
-
-            if (currPosition != Vector3.zero)
+            if (currPosition != Vector3.zero)                       // 현재 컨트롤러의 위치값이 있으면 이전 위치에 넣음
                 prevPosition = currPosition;
 
-            currPosition = controller.transform.position;
-            velocity = (currPosition - prevPosition).normalized;
-            if (firstDirection == Vector3.zero)
+            currPosition = controller.transform.position;           // 현재 컨트롤러의 위치값 가져옴
+            velocity = (currPosition - prevPosition).normalized;    // 컨트롤러의 방향벡터 구하기
+            if (firstDirection == Vector3.zero && Math.Abs(velocity.y) < 0.5)                     // 컨트롤러 초기 방향벡터가 없고, x,z평면과 y축이 30도 미만일 때만 해당 방향벡터를 방향벡터로 넣어줌
             {
-                firstDirection = velocity;
+              
+                    firstDirection = velocity;
             }
-            horizontal = Vector3.Dot(firstDirection, velocity);
-
-            //(Math.Abs(firstDirection.y) < 1 && input == 1)
+            horizontal = Vector3.Dot(firstDirection, velocity);     // 초기 방향벡터와 현재 방향벡터 내적
 
 
-            Debug.Log(horizontal.ToString("F3"));
-            if (horizontal > 0.7f || horizontal < -0.7f && ac.state == AnimalControl.State.Handle)
+
+            // Debug.Log($"dot product : {horizontal.ToString("F3")}");   // Dot product 디버그
+
+            float speed = (currPosition - prevPosition).magnitude / Time.deltaTime;
+            Debug.Log($"Shake Speed : {speed.ToString("F3")}"); // Velocity 디버그
+
+            if ((horizontal > 0.7f || horizontal < -0.7f) && ac.state == AnimalControl.State.Handle && speed > 200f)
             {
+                if (delay_co != null)
+                    StopCoroutine(delay_co);
                 ap.isPetting = true;
+
             }
             else
             {
-                ap.isPetting = false;
-
-                // delay_co = StartCoroutine(DelayResetData(false));
-                ResetData(false);
+                delay_co = StartCoroutine(DelayResetData(false));
+                //ResetData(false);
             }
         }
         else
-        ResetData(false);
-            //delay_co = StartCoroutine(DelayResetData(false));
+        {
+            // ResetData(false);
+            delay_co = StartCoroutine(DelayResetData(false));
+        }
     }
 
-    void OnTriggerExit(Collider other)
+    void OnTriggerExit(Collider other)          
     {
-        ResetData(true);        
+        //ResetData(true);
+        delay_co = StartCoroutine(DelayResetData(true));
     }
 
 
 
-    // IEnumerator DelayResetData(bool disconnect)
-    // {
-    //     yield return new WaitForSeconds(2f);
+    IEnumerator DelayResetData(bool disconnect)     // 2초동안 행동 안할 시 값 초기화
+    {
+        yield return new WaitForSeconds(2f);
 
-    //     prevPosition = Vector3.zero;
-    //     currPosition = Vector3.zero;
-    //     firstDirection = Vector3.zero;
-    //     isStable = false;
-    //     if (disconnect)
-    //     {
-    //         controller = null;
-    //     }
-    // }
+        prevPosition = Vector3.zero;
+        currPosition = Vector3.zero;
+        firstDirection = Vector3.zero;
+        isStable = false;
+        ap.isPetting = false;
+        if (disconnect)
+        {
+            controller = null;
+        }
+    }
     void ResetData(bool disconnect)
     {
         prevPosition = Vector3.zero;
@@ -113,7 +120,4 @@ public class PettingTrigger : MonoBehaviour
             controller = null;
         }
     }
-  
-
-
 }
