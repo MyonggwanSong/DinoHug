9a7931d754d3eeaddbd4b573301ed785491f3e79
HyejeongOk <oghyejeong2@gmail.com>
2025-07-09 18:01:17 +0900

    OHJ] v0.0.6 공던지기 구현완료

--- Assets/_OHJ/Scripts/AnimalPlay.cs
+++ Assets/_OHJ/Scripts/AnimalPlay.cs
@@ -8,11 +8,10 @@ public class AnimalPlay : AnimalAbility
     [SerializeField] float stopDistance = 1.5f;
     private NavMeshAgent agent;
     public Transform ballpos;
-    public GameObject carry;
-
-    public GameObject player;
+   [ReadOnlyInspector] [SerializeField] Transform playerCam;
 
     private bool isPlay = false;
+    public bool isBite = false;
 
     /*
      * ���� ������ ĳ���ʹ� ���� ���󰡼� ���� �ֿ췯 ����.
@@ -21,9 +20,10 @@ public class AnimalPlay : AnimalAbility
      */
     protected override void Awake()
     {
-        base.Awake();
-        TryGetComponent(out agent);
-        player = GameObject.FindGameObjectWithTag("Player");
+            base.Awake();
+            TryGetComponent(out agent);
+            playerCam = Camera.main.transform;
+            target.TryGetComponent(out toy);
     }
 
     public override void Init()
@@ -38,121 +38,96 @@ public class AnimalPlay : AnimalAbility
         agent.isStopped = true;
     }
 
-    public GameObject target;
+    public Transform target;
+    Toy toy;
     public Collider[] Collider;
     private IEnumerator PlayBall()
     {
-        isPlay = true;
-        animal.state = AnimalControl.State.Play;
-        // ���� ����
-        Collider = Physics.OverlapSphere(transform.position, 80f);
-
-        // ��ã��
-        for (int i = 0; i < Collider.Length; i++)
+        // 플레이어에게 다가오는거 구현
+        // 공까지 가는 동안 대기
+        while (true)
         {
-            if (Collider[i].CompareTag("Ball"))
-
+            //  공하고 stopDistance 보다 멀면 계속 플레이어 추적, 너무 가까우면 루프문에서 대기는 하는데 추적은 안함
+            agent.isStopped = false;
+            agent.SetDestination(playerCam.position);
+            while (true)
             {
-                target = Collider[i].gameObject;
+
+                if ((target.position - transform.position).sqrMagnitude < stopDistance * stopDistance)
+                {
+                    agent.isStopped = true;
+                    break;
+                }
+                else
+                {
+                    agent.isStopped = false;
+                }
+                if (toy.isThrow) break;
+                yield return null;
+
+
+            }
+            if (toy.isThrow) break;
+            yield return new WaitForSeconds(0.2f);
+        }
+
+        // 던져진 공을 쫒아간다.
+        // 플레이어에게 다가오는거 구현
+        // 공까지 가는 동안 대기
+        float startTime = Time.time;
+        Rigidbody ball_rb = target.GetComponent<Rigidbody>();
+        while (true)
+        {
+            agent.SetDestination(target.position);
+            yield return new WaitForSeconds(0.2f);
+            if (Time.time - startTime > 1f && ((target.position - transform.position).sqrMagnitude < stopDistance * stopDistance))
+            {
                 break;
             }
         }
 
-        // ������
-        agent.SetDestination(target.transform.position);
+        Debug.Log(" 던져진 공에 도착 완료했습니다.");
+
+
+        // 공을 ballPos 에 자식으로 부착하는데 이때 공의 rigidbody나 움직이던거를 다 정지로 초기화 해야함. 선택사항) 이때 이 공을 XR 그랩 못하게함
+        yield return new WaitForSeconds(1f);
+
+        agent.isStopped = true;
+        target.SetParent(ballpos);
+        Debug.Log("입에 넣기");
+
+
+        yield return new WaitForSeconds(1f);
 
         while(true)
         {
-            float dist = Vector3.Distance(transform.position, target.transform.position);
-            Debug.Log(dist);
-            if(dist < stopDistance)
+            agent.isStopped = false;
+            Debug.Log("1.플레이어에게");
+            agent.SetDestination(playerCam.position);
+            target.localPosition = Vector3.zero;
+            Debug.Log("간다");
+            if (Vector3.Distance(transform.position, playerCam.position) < stopDistance + 1f)
             {
-                agent.isStopped = true;
+                target.SetParent(null);
+                target.position += transform.forward;
+                ball_rb.isKinematic = false;
+                toy.isThrow = false;
+                Debug.Log("놀아주기 끝");
                 break;
             }
             yield return null;
         }
+        
+       
 
-
-        //yield return new WaitForSeconds(2f);
-        //yield return new WaitUntil(() => Vector3.Distance(transform.position, target.transform.position) < stopDistance);
-        //Debug.Log($"{stopDistance} �տ��� ����");
-        //agent.isStopped = true;
-
-
-
-        // �Ÿ��� ������� ��
-        if (Vector3.Distance(transform.position, target.transform.position) < 30f)
-        {
-            // target�� carry��� �ӽ� ������ ����
-            carry = target;
-
-            carry.transform.SetParent(ballpos);
-
-            //�ڿ������� �����̱�
-            float elapsed = 0f;
-            float duration = 1f;
-
-            while(elapsed < duration)
-            {
-                elapsed += Time.deltaTime;
-                float t = elapsed / duration;
-                carry.transform.localPosition = Vector3.Lerp(carry.transform.localPosition, new Vector3(0.01f, 0.1f, 0.5f), t);
-                yield return null;
-            }
-
-            //�� �߷� ����
-            Rigidbody ball_rb = carry.GetComponent<Rigidbody>();
-            if (ball_rb != null) ball_rb.isKinematic = true;
-            
-        }
-
-        //if(Vector3.Distance(transform.position, target.transform.position) < 2f)
-        //{
-        //    carry = target;
-
-        //    carry.transform.SetParent(ballpos);
-        //    carry.transform.localPosition = Vector3.Lerp(carry.transform.position, new Vector3(0.01f, 0.1f, 0.5f), 2f);
-
-        //    // �� �߷� ����
-        //    Rigidbody ball_rb = carry.GetComponent<Rigidbody>();
-        //    if(ball_rb != null)
-        //        ball_rb.isKinematic = true;
-
-        //    yield return null;
-
-        //    //�ִϸ��̼� ����
-
-        //    //carry.transform.localPosition = new Vector3(-1f, 0f, -0.5f);
-
-
-        //    Debug.Log("1");
-        //    // 2�� ���� ����
-        //    Debug.Log("2");
-
-        //    yield return new WaitForSeconds(3f);
-
-        //    target = player;
-        //    Debug.Log("3");
-
-        //    agent.SetDestination(target.transform.position);
-
-        //    while (Vector3.Distance(transform.position, target.transform.position) < 2f)
-        //    {
-        //        ball_rb.isKinematic = false;
-
-        //        yield return null;
-        //        //�ִϸ��̼� ����
-        //    }
-
-
-        //}
-        isPlay = false;
-
-
-        Debug.Log("���� ����");
+        // 종료
         animal.ChangeState(AnimalControl.State.Idle);
 
-        //animal.ChangeState(AnimalControl.State.Play);
+
+
+
+
+
+
     }
 }
